579f524c322729ee477208129bec3e643b6ff29d
juliiii <161629965+julieetta1166@users.noreply.github.com>
2025-04-07 11:56:02 -0300

    Create codigos.py

---
+++ codigos.py
@@ -0,0 +1,7 @@
+import numpy as np
+import pandas as pd
+provincia=['Cordoba', 'Mendoza', 'Santa Fe', 'Buenos Aires']
+poblacion=[ '700', '800', '900', '1000']
+diccionario= {"PROVINCIA":provincia,"POBLACION":poblacion}
+df= pd.DataFrame (diccionario)
+print (df)
