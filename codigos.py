fbf6b29fb927e67e8b02ad3ad2674187aef956ca
juliiii <161629965+julieetta1166@users.noreply.github.com>
2025-04-07 12:06:26 -0300

    Update codigos.py

--- codigos.py
+++ codigos.py
@@ -1,3 +1,4 @@
+COMO CREAR FILAS Y COLUMNAS
 import numpy as np
 import pandas as pd
 provincia=['Cordoba', 'Mendoza', 'Santa Fe', 'Buenos Aires']
@@ -5,3 +6,8 @@ poblacion=[ '700', '800', '900', '1000']
 diccionario= {"PROVINCIA":provincia,"POBLACION":poblacion}
 df= pd.DataFrame (diccionario)
 print (df)
+
+COMO ABRIR UN CSV 
+import pandas as pd
+df=pd.read_csv ('StudentsPerformance.csv')
+print(df)
