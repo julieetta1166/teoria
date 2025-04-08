2c1292058198e44ec417d906dea45a0e34e4ae9f
juliiii <161629965+julieetta1166@users.noreply.github.com>
2025-04-08 12:42:42 -0300

    Update 07-04.py

--- ACTIVIDAAAAADDD/07-04.py
+++ ACTIVIDAAAAADDD/07-04.py
@@ -22,3 +22,13 @@ Mostar las 3 ultimas columnas
 Mostrar las primeras 100 filas
 Mostrar la media, el minimo, maximo y cantidad
 Convertir a string
+
+import pandas as pd
+df=pd.read_csv ('StudentsPerformance.csv')
+print(df[['gender',"math score"]])
+print(df[['gender','lunch','math score']].head) #Como mostrar dos columnas
+print(df.head(100)) #Muestra las primeras filas
+print(df.cant())
+print(df.max())
+print(df.min())
+print(df.mean()) #Muestra la media
