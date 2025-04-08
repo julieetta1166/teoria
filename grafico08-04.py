6b5d92a01cc24d88b2b36f393731b5c02a3a8ceb
juliiii <161629965+julieetta1166@users.noreply.github.com>
2025-04-08 13:48:58 -0300

    Update grafico08-04.py

--- grafico08-04.py
+++ grafico08-04.py
@@ -1,4 +1,4 @@
-GRAFICO BASICO
+GRAFICO LINEAL
 import matplotlib.pyplot as plt #importar libreria
 x=[ 1, 2,3,4,5]   #datos
 y=[ 11, 16, 20,13, 17] #datos
@@ -7,3 +7,12 @@ plt.xlabel('eje x') #etiqueta
 plt.ylabel('eje y') #etiqueta
 plt.title('Grafico de ejemplo') #titulo
 plt.show() #mostrar graficos
+
+GRAFICO CIRCULAR
+import matplotlib.pyplot as plt #importar libreria
+label=[ 'a','b','c','d']   #datos
+datos=[ 25,35,20,20] #datos
+colores=['gold','lightcoral','lightskyblue','lightgreen']
+plt.pie(datos,labels=label,colors=colores,autopct='%1.1f%%',startangle=140)
+plt.axis('equal')
+plt.show()
