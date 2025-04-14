4dd33332788b4f33d691a2c12de4f9711ad92728
juliiii <161629965+julieetta1166@users.noreply.github.com>
2025-04-14 14:19:44 -0300

    Update COLUMNANUEVA.py

--- COLUMNANUEVA.py
+++ COLUMNANUEVA.py
@@ -20,6 +20,12 @@ a=np.random.randint(1,100,size=1000)
 df['columna de arreglo']=a
 print(df)
 
+SUMANDO LOS NUMEROS DE LA COLUMNA DRE MATH SCORE
+import pandas as pd
+import numpy as np
+df=pd.read_csv ('StudentsPerformance.csv')
+print(df['math score'].sum)
+print(df.head) 
 CREAR NUMEROS DECIMALES
 import pandas as pd
 import numpy as np
