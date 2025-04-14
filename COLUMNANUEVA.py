6d940ff8370464b4da27fdf66848a934feb73aca
juliiii <161629965+julieetta1166@users.noreply.github.com>
2025-04-14 13:59:42 -0300

    Update COLUMNANUEVA.py

--- COLUMNANUEVA.py
+++ COLUMNANUEVA.py
@@ -3,3 +3,11 @@ import pandas as pd
 df=pd.read_csv ('StudentsPerformance.csv')
 df['nueva columna']= 70
 print(df)
+
+COLUMNA DE ARREGLO
+import pandas as pd
+import numpy as np
+df=pd.read_csv ('StudentsPerformance.csv')
+a=np.arange(0,1000)
+df['columna de arreglo']=a
+print(df)
