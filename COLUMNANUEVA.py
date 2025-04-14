7a729029c12e3bca6e831ffc0fb4ea8f044b1233
juliiii <161629965+julieetta1166@users.noreply.github.com>
2025-04-14 14:11:44 -0300

    Update COLUMNANUEVA.py

--- COLUMNANUEVA.py
+++ COLUMNANUEVA.py
@@ -19,3 +19,12 @@ df=pd.read_csv ('StudentsPerformance.csv')
 a=np.random.randint(1,100,size=1000)
 df['columna de arreglo']=a
 print(df)
+
+CREAR NUMEROS DECIMALES
+import pandas as pd
+import numpy as np
+df=pd.read_csv ('StudentsPerformance.csv')
+a=np.random.uniform(1,100,size=1000)
+df['columna de arreglo']=a
+print(df)
+
