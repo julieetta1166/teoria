9a1a81d5c0ca285af38fa08d802b99f9f0d3e4c6
juliiii <161629965+julieetta1166@users.noreply.github.com>
2025-04-14 14:07:48 -0300

    Update COLUMNANUEVA.py

--- COLUMNANUEVA.py
+++ COLUMNANUEVA.py
@@ -11,3 +11,11 @@ df=pd.read_csv ('StudentsPerformance.csv')
 a=np.arange(0,1000)
 df['columna de arreglo']=a
 print(df)
+
+COLUMNA CON NUMEROS ALEATORIOS
+import pandas as pd
+import numpy as np
+df=pd.read_csv ('StudentsPerformance.csv')
+a=np.random.randint(1,100,size=1000)
+df['columna de arreglo']=a
+print(df)
