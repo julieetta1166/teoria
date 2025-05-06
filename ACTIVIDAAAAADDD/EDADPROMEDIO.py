b23982cbbf41622387e6a02bb5a5919599bba17b
juliiii <161629965+julieetta1166@users.noreply.github.com>
2025-05-06 14:44:55 -0300

    Update EDADPROMEDIO.py

--- ACTIVIDAAAAADDD/EDADPROMEDIO.py
+++ ACTIVIDAAAAADDD/EDADPROMEDIO.py
@@ -21,3 +21,21 @@ plt.ylabel("Edad Promedio")
 plt.xticks(rotation=0)
 plt.tight_layout()
 plt.show()
+
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+df= pd.read_csv("StudentsPerformance.csv")
+conteos, bins =np.histogram(df["math score"], bins=10)
+bin_centers =0.5* (bins[1:] + bins[:-1]) 
+anchos=np.diff(bins) 
+colores=['gold','lightcoral','lightskyblue','lightgreen','red','blue','pink','purple','yellow']
+plt.figure(figsize=(8, 5))
+plt.bar(bin_centers, conteos, width=anchos, color=colores, edgecolor='black')
+plt.title("Distribución de puntajes de Matemáticas")
+plt.xlabel("Puntaje")
+plt.ylabel("Cantidad de estudiantes")
+plt.grid(axis='y', linestyle=' ', alpha=0.7)
+plt.tight_layout()
+plt.show()
