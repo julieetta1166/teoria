12ceeb9cb61e8bbe1596935e9588c93cbe625721
juliiii <161629965+julieetta1166@users.noreply.github.com>
2025-05-19 13:40:51 -0300

    Create PRUEBA.py

---
+++ PRUEBA.py
@@ -0,0 +1,3 @@
+19-05-25
+Prueba
+1. Obtener el nombre de todos los usuarios mayores a 20 años de los siguintes paises: España, Francia y Turquia. Cuantos son masculinos y cvuiatos femeninos, y graficar
