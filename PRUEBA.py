7479d328920e833ddee3d27b81cb8407cc690aa1
juliiii <161629965+julieetta1166@users.noreply.github.com>
2025-05-19 13:42:19 -0300

    Update PRUEBA.py

--- PRUEBA.py
+++ PRUEBA.py
@@ -1,3 +1,4 @@
 19-05-25
 Prueba
 1. Obtener el nombre de todos los usuarios mayores a 20 años de los siguintes paises: España, Francia y Turquia. Cuantos son masculinos y cvuiatos femeninos, y graficar
+2. Obtener todos los usuarios entre las edades de 45 y 60. Cuntos son asculinos y cuantos son femeninos, hacer un grafico con matlobit
